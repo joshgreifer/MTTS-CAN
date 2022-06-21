1aeb0aeb022cced2c682119d675904b6bc26afe8
Josh Greifer <joshgreifer@gmail.com>
2022-06-21 12:11:53 +0100

    First commit of webcam capture - omit --video_path arg to use cam TODO: Fix new capture window being generated every second

--- code/inference_preprocess.py
+++ code/inference_preprocess.py
@@ -12,26 +12,43 @@ def preprocess_raw_video(videoFilePath, dim=36):
     # set up
     t = []
     i = 0
-    vidObj = cv2.VideoCapture(videoFilePath)
-    totalFrames = int(vidObj.get(cv2.CAP_PROP_FRAME_COUNT)) # get total frame size
+    if videoFilePath is None:
+        cap = cv2.VideoCapture(0)
+        # Check success
+        if not cap.isOpened():
+            raise Exception("Could not open video device")
+        # Set properties. Each returns === True on success (i.e. correct resolution)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        cap.set(cv2.CAP_PROP_FPS, 60)
+        frame_rate = cap.get(cv2.CAP_PROP_FPS)
+        totalFrames = int(frame_rate * 30)
+    else:
+        cap = cv2.VideoCapture(videoFilePath)
+        totalFrames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) # get total frame size
     Xsub = np.zeros((totalFrames, dim, dim, 3), dtype=np.float32)
-    height = vidObj.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    width = vidObj.get(cv2.CAP_PROP_FRAME_WIDTH)
-    success, img = vidObj.read()
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    success, img = cap.read()
     dims = img.shape
     print("Orignal Height", height)
     print("Original width", width)
     #########################################################################
     # Crop each frame size into dim x dim
-    while success:
-        t.append(vidObj.get(cv2.CAP_PROP_POS_MSEC))  # current timestamp in milisecond
+    while success and i < totalFrames:
+        t.append(cap.get(cv2.CAP_PROP_POS_MSEC))  # current timestamp in milisecond
         vidLxL = cv2.resize(img_as_float(img[:, int(width/2)-int(height/2 + 1):int(height/2)+int(width/2), :]), (dim, dim), interpolation=cv2.INTER_AREA)
         vidLxL = cv2.rotate(vidLxL, cv2.ROTATE_90_CLOCKWISE)  # rotate 90 degree
         vidLxL = cv2.cvtColor(vidLxL.astype('float32'), cv2.COLOR_BGR2RGB)
         vidLxL[vidLxL > 1] = 1
         vidLxL[vidLxL < (1/255)] = 1/255
         Xsub[i, :, :, :] = vidLxL
-        success, img = vidObj.read() # read the next one
+        success, img = cap.read() # read the next one
+        if videoFilePath is None:
+            cv2.imshow(f'Frame {i // frame_rate}', img)
+            if cv2.waitKey(1) & 0xFF == 27:
+                break
+
         i = i + 1
     plt.imshow(Xsub[0])
     plt.title('Sample Preprocessed Frame')
